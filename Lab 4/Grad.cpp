6becedc8c2a8207c8d1a95ea7728695587152262
Josh Hazel <joshhazel@sbcglobal.net>
2012-02-11 22:31:14 -0800

    first commit

---
+++ Lab 4/Grad.cpp
@@ -0,0 +1,32 @@
+/*
+Joshua Hazel
+CSCD305 C++
+Lab 4 Inheritance
+*/
+
+#include "Grad.h"
+#include "main.h"
+
+CGrad::CGrad(string s, int i, int cr, double fee) : CStudent(s,i,cr) 
+{
+	this->fee = fee;
+}//end constructor
+
+CGrad::CGrad(const CGrad & orig) {
+	this->setName(orig.getName());
+	this->setID(orig.getID());
+	this->setCredits(orig.getCredits());
+}//end copy constructor
+
+CGrad::~CGrad() {
+
+}//end destructor
+
+double CGrad::calcFees()const {
+	return CStudent::calcFees() + (CStudent::getCredits() * CGrad::fee);
+}//end calcFees
+
+void CGrad::print(ostream & out)const {
+	CStudent::print(out);
+	out<<"Fees: "<<this->calcFees()<<endl;
+}//end print
---
+++ Lab 4/Grad.h
@@ -0,0 +1,26 @@
+/*
+Joshua Hazel
+CSCD305 C++
+Lab 4 Inheritance
+*/
+
+#pragma once
+#include "main.h"
+#include "Student.h"
+
+class CGrad : public CStudent
+{
+public:
+	CGrad(string s="Ima Nerd", int i=123456, int cr=12, double fee = 202.50);
+	CGrad(const CGrad & orig);
+	virtual ~CGrad();
+
+	double calcFees()const;
+	void print(ostream & out)const;
+
+	friend ostream & operator<<(ostream & out, const CGrad & rhs);
+
+private:
+	double fee;
+};
+
